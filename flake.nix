ad47ecc672e0e83697ec01f7a59f942ec1a8ec6d
Nate Smith <nate@theinternate.com>
2022-04-28 17:11:24 -0400

    Flakify

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ruby_3_1
+          ];
+        };
+      }
+    );
+}
